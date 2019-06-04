5ea0e308497ddaf2333b7d4122977156324fdcc6
Admin <Admin@DESKTOP-89NTJEN>
2019-06-05 11:32:25 +1200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace addingAudio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // addinng a wav audio using resource file
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer sndplayer = new SoundPlayer(addingAudio.Properties.Resources.Charging_tone);
+                sndplayer.Play();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show ("Error Message" + ex.Message);
+            }
+        }
+    }
+
+}
